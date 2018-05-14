3948342169b25339eb82348fc02045197d2a75ca
Carlos Sanchez Romero <carles.sanchez@atmira.com>
2018-05-14 08:33:01 +0200

    Begin app

---
+++ XSLTTransform.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Xsl;
+
+namespace XSLTTransform.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new XslCompiledTransform and load the compiled transformation.
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load(typeof(Transform));
+
+            // Execute the transformation and output the results to a file.
+            xslt.Transform("books.xml", "discount_books.html");
+        }
+    }
+}
